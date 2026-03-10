bd65580c048529d90a7f9bdd2f635a59ef819d1f
SmartGeo-DevTeam <smartgeo.dev@gmail.com>
2026-03-10 13:42:23 +0900

    init

---
+++ src/_lib/fonts.ts
@@ -0,0 +1,26 @@
+import localFont from 'next/font/local';
+
+export const SpoqaHanSansNeo = localFont({
+  src: [
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo/700.woff2',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo/500.woff2',
+      weight: '500',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo/400.woff2',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo/300.woff2',
+      weight: '300',
+    },
+    {
+      path: '../../public/fonts/SpoqaHanSansNeo/100.woff2',
+      weight: '100',
+    },
+  ],
+});
