d27e7b6dd356d136a7b24ff5a1b99186bd85a513
SmartGeo-DevTeam <smartgeo.dev@gmail.com>
2026-05-20 14:34:19 +0900

    추가: 글로벌청맥 섹션

--- src/app/[lang]/_components/home/index.tsx
+++ src/app/[lang]/_components/home/index.tsx
@@ -3,6 +3,7 @@ import HomeNavs from './2_navs';
 import HomeSpecialties from './3_specialties';
 import HomeReviews from './4_reviews';
 import HomeDoctors from './5_doctors';
+import HomeGlobal from './6_global';
 
 export default function HomeSections() {
   return (
@@ -12,6 +13,7 @@ export default function HomeSections() {
       <HomeSpecialties />
       <HomeReviews />
       <HomeDoctors />
+      <HomeGlobal />
     </>
   );
 }
