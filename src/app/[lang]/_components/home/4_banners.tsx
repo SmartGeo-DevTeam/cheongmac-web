f998b585aca786d0e01135238b5edfca2a3caea0
SmartGeo-DevTeam <smartgeo.dev@gmail.com>
2026-06-04 17:03:16 +0900

    메인페이지 배너 섹션 구조

--- src/app/[lang]/_components/home/4_banners.tsx
+++ src/app/[lang]/_components/home/4_banners.tsx
@@ -1,5 +1,28 @@
-'use client';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function HomeBanners() {
-  return <section></section>;
+  return (
+    <section className="px-5 pt-10 pb-15">
+      <Link
+        target="_blank"
+        href={`/`}
+        className="relative flex w-full aspect-335/136
+        xl:mx-auto xl:max-w-7xl xl:aspect-1280/317"
+      >
+        <Image
+          src="/images/home/banners/m-item-1.png"
+          alt="m-item-1"
+          fill
+          className="block xl:hidden"
+        />
+        <Image
+          src="/images/home/banners/pc-item-1.png"
+          alt="pc-item-1"
+          fill
+          className="hidden xl:block"
+        />
+      </Link>
+    </section>
+  );
 }
