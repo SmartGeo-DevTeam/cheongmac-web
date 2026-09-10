9db6974576659585632b0c5ff4aea866c7a9dbaf
SmartGeo-DevTeam <smartgeo.dev@gmail.com>
2026-09-10 17:39:56 +0900

    변경: 폰트 에셋 경로 정리

--- src/_lib/fonts.ts
+++ src/_lib/fonts.ts
@@ -3,39 +3,39 @@ import localFont from 'next/font/local';
 export const suit = localFont({
   src: [
     {
-      path: '../../public/fonts/suit/900.woff2',
+      path: '../_assets/fonts/suit/900.woff2',
       weight: '900',
     },
     {
-      path: '../../public/fonts/suit/800.woff2',
+      path: '../_assets/fonts/suit/800.woff2',
       weight: '800',
     },
     {
-      path: '../../public/fonts/suit/700.woff2',
+      path: '../_assets/fonts/suit/700.woff2',
       weight: '700',
     },
     {
-      path: '../../public/fonts/suit/600.woff2',
+      path: '../_assets/fonts/suit/600.woff2',
       weight: '600',
     },
     {
-      path: '../../public/fonts/suit/500.woff2',
+      path: '../_assets/fonts/suit/500.woff2',
       weight: '500',
     },
     {
-      path: '../../public/fonts/suit/400.woff2',
+      path: '../_assets/fonts/suit/400.woff2',
       weight: '400',
     },
     {
-      path: '../../public/fonts/suit/300.woff2',
+      path: '../_assets/fonts/suit/300.woff2',
       weight: '300',
     },
     {
-      path: '../../public/fonts/suit/200.woff2',
+      path: '../_assets/fonts/suit/200.woff2',
       weight: '200',
     },
     {
-      path: '../../public/fonts/suit/100.woff2',
+      path: '../_assets/fonts/suit/100.woff2',
       weight: '100',
     },
   ],
@@ -44,35 +44,35 @@ export const suit = localFont({
 export const notoSerifKR = localFont({
   src: [
     {
-      path: '../../public/fonts/noto-serif-kr/900.woff2',
+      path: '../_assets/fonts/noto-serif-kr/900.woff2',
       weight: '900',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/800.woff2',
+      path: '../_assets/fonts/noto-serif-kr/800.woff2',
       weight: '800',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/700.woff2',
+      path: '../_assets/fonts/noto-serif-kr/700.woff2',
       weight: '700',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/600.woff2',
+      path: '../_assets/fonts/noto-serif-kr/600.woff2',
       weight: '600',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/500.woff2',
+      path: '../_assets/fonts/noto-serif-kr/500.woff2',
       weight: '500',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/400.woff2',
+      path: '../_assets/fonts/noto-serif-kr/400.woff2',
       weight: '400',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/300.woff2',
+      path: '../_assets/fonts/noto-serif-kr/300.woff2',
       weight: '300',
     },
     {
-      path: '../../public/fonts/noto-serif-kr/200.woff2',
+      path: '../_assets/fonts/noto-serif-kr/200.woff2',
       weight: '200',
     },
   ],
@@ -81,7 +81,7 @@ export const notoSerifKR = localFont({
 export const jejuMyeongjo = localFont({
   src: [
     {
-      path: '../../public/fonts/jeju-myeongjo/400.woff2',
+      path: '../_assets/fonts/jeju-myeongjo/400.woff2',
       weight: '400',
     },
   ],
